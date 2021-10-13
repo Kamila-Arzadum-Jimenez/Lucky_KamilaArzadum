07a5b3187cfef10c0d25529abb7dacab38d37729
Kamila-Arzadum-Jimenez <a2020111543@estudiantes.upsa.edo.bo>
2021-10-13 19:25:48 -0400

    Agregar archivos de proyecto.

---
+++ Parcial2_KamilaArzadum/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Parcial2_KamilaArzadum.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parcial2_KamilaArzadum.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        // Agregar los modelos a utilizar
+        public DbSet<Suerte> Suerte { get; set; }
+    }
+
+   
+}
---
+++ Parcial2_KamilaArzadum/Models/Suerte.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parcial2_KamilaArzadum.Models
+{
+    public class Suerte
+    {
+        [Key]
+        public int SuerteId { get; set; }
+        [StringLength(maximumLength: 10, MinimumLength = 1, ErrorMessage = "El numero de la suerte debe ir del 0 al 10")]
+        
+        public string Detalle { get; set; }
+
+        [Url]
+        [StringLength(100, MinimumLength = 10, ErrorMessage = "La longitud de {0} debe estar entre {2} y {1}")]
+        
+        public string Imagen { get; set; }
+    }
+}
